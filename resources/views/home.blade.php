8d9a3af25647a1095e473c316e40ed5fd2be0294
unknown <meiradwianaa@gmail.com>
2021-11-29 20:55:08 +0700

    restoapp

---
+++ resources/views/home.blade.php
@@ -0,0 +1,60 @@
+@extends('layouts.main')
+
+@section('container')
+<div class="landing img-fluid">
+    <div class="container">
+        <div class="row">
+            <div class="col-6 ms-3">
+                <h1>Laboriosam dolor sequi ut iure</h1>
+                <p>Laboriosam dolor sequi ut iure</p>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-4">
+                <h2>Download<img src="./img/download.png" height="30"></h2>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+        <div class="row">
+            <div class="col-sm-4"></div>
+            <div class="col-sm-2">
+                <img src="./img/qr.png" height="150">
+                <div class="vl"></div>
+            </div>
+            <div class="col-sm-2">
+                <div class="buttondwonload">
+                    <a class="btn btn-google" href="#" title="GooglePlay">Google Play</a>
+                    <a class="btn btn-appstore" href="#" title="AppsStore">Apps Store</a>
+                    <a class="btn btn-computer" href="#" title="Computer">Your Computer</a>
+                </div>
+            </div>
+            <div class="col-sm-4"></div>
+        </div>
+    </div>
+</div>
+<div class="layanan img-fluid">
+    <h2 class="subtittle">Layanan </h2>
+    <div class="row">
+            <div class="col-sm-7">
+            </div>
+            <div class="col-sm-5 right">
+                <h2 >Dine In</h2>
+                <p >Quis doloribus porro adipisci doloremque dolores consequatur iste sit. Incidunt consequatur voluptatem aliquid et autem.
+                Quis doloribus porro adipisci doloremque dolores consequatur iste sit. Incidunt consequatur voluptatem aliquid et autem.
+                Quis doloribus porro adipisci doloremque dolores consequatur iste sit. Incidunt consequatur voluptatem aliquid et autem.<p>
+            </div>
+    </div>
+    <div class="row">
+            <div class="col-sm-6 left">
+            <h2 >Delivery</h2>
+                <p >Quis doloribus porro adipisci doloremque dolores consequatur iste sit. Incidunt consequatur voluptatem aliquid et autem.
+                Quis doloribus porro adipisci doloremque dolores consequatur iste sit. Incidunt consequatur voluptatem aliquid et autem.
+                Quis doloribus porro adipisci doloremque dolores consequatur iste sit. Incidunt consequatur voluptatem aliquid et autem.<p>
+            </div>
+            <div class="col-sm-6"></div>
+    </div>
+</div>
+
+
+@endsection
---
+++ resources/views/partials/navbar.blade.php
@@ -0,0 +1,23 @@
+<nav class="navbar navbar-expand-lg shadow p-3 mb-5 rounded" >
+    <div class="container">
+      <a class="navbar-brand" href="/">
+        <img src={{ asset("./img/logo.png") }} alt="logo" height="50">
+      </a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="navbar-collapse collapse" style="font-color:#000000">
+        <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="#">Unduh</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" >Layanan</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#" >Fitur</a>
+            </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
